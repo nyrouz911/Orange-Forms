754c4a72a1cc3333a8ea061def7ce0c0bbe879af
nyrouz911 <nayrouz.tebib@esprit.tn>
2025-09-10 01:23:36 +0200

    ready for deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
 import PlausibleProvider from 'next-plausible'
 
 const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
